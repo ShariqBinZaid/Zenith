4db3aa20200f9d8a8229417e9794632eef3c31f5
FahadAli6026 <122897455+FahadAli6026@users.noreply.github.com>
2023-01-27 20:42:45 +0500

    repositioning dashboard grids

--- resources/views/dashboard/admin.blade.php
+++ resources/views/dashboard/admin.blade.php
@@ -2,6 +2,126 @@
 
 @section('content')
 <div class="row row-cols-1 row-cols-md-3 g-4">
+<div class="col-lg-3 col-md-12">
+                <div class="card h-100">
+                    <div class="card-body">
+                        <div class="d-flex mb-3">
+                            <div class="display-7">
+                                <i class="bi bi-telephone-inbound"></i>
+                                <!-- <i class="bi bi-basket"></i> -->
+                            </div>
+                            <div class="dropdown ms-auto">
+                                <!-- <a href="#" data-bs-toggle="dropdown" class="btn btn-sm" aria-haspopup="true"
+                                aria-expanded="false">
+                                    <i class="bi bi-three-dots"></i>
+                                </a> -->
+                                <div class="dropdown-menu dropdown-menu-end">
+                                    <a href="#" class="dropdown-item">View Detail</a>
+                                    <a href="#" class="dropdown-item">Download</a>
+                                </div>
+                            </div>
+                        </div>
+                        <h4 class="mb-3">Total Leads</h4>
+                        <div class="d-flex mb-3">
+                            <div class="display-7">{{$totalead}}</div>
+                            <div class="ms-auto" id="total-orders"></div>
+                        </div>
+                        <div class="text-success">
+                            Over last month 1.4% <i class="small bi bi-arrow-up"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-12">
+                <div class="card h-100">
+                    <div class="card-body">
+                        <div class="d-flex mb-3">
+                            <div class="display-7">
+                                <i class="bi bi-briefcase"></i>
+                                <!-- <i class="bi bi-credit-card-2-front"></i> -->
+                            </div>
+                            <div class="dropdown ms-auto">
+                                <!-- <a href="#" data-bs-toggle="dropdown" class="btn btn-sm" aria-haspopup="true"
+                                aria-expanded="false">
+                                    <i class="bi bi-three-dots"></i>
+                                </a> -->
+                                <div class="dropdown-menu dropdown-menu-end">
+                                    <a href="#" class="dropdown-item">View Detail</a>
+                                    <a href="#" class="dropdown-item">Download</a>
+                                </div>
+                            </div>
+                        </div>
+                        <h4 class="mb-3">Total Opportunities</h4>
+                        <div class="d-flex mb-3">
+                            <div class="display-7">{{$totalopportunity}}</div>
+                            <div class="ms-auto" id="total-opportunity"></div>
+                        </div>
+                        <div class="text-warning">
+                            Over last month 2.4% <i class="small bi bi-arrow-down"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-12">
+                <div class="card h-100">
+                    <div class="card-body">
+                        <div class="d-flex mb-3">
+                            <div class="display-7">
+                                <i class="bi bi-briefcase"></i>
+                                <!-- <i class="bi bi-credit-card-2-front"></i> -->
+                            </div>
+                            <div class="dropdown ms-auto">
+                                <!-- <a href="#" data-bs-toggle="dropdown" class="btn btn-sm" aria-haspopup="true"
+                                aria-expanded="false">
+                                    <i class="bi bi-three-dots"></i>
+                                </a> -->
+                                <div class="dropdown-menu dropdown-menu-end">
+                                    <a href="#" class="dropdown-item">View Detail</a>
+                                    <a href="#" class="dropdown-item">Download</a>
+                                </div>
+                            </div>
+                        </div>
+                        <h4 class="mb-3">Total Packages</h4>
+                        <div class="d-flex mb-3">
+                            <div class="display-7">{{$totalpackage}}</div>
+                            <div class="ms-auto" id="total-opportunity"></div>
+                        </div>
+                        <div class="text-danger">
+                            Over last month 2.4% <i class="small bi bi-arrow-down"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-12">
+                <div class="card h-100">
+                    <div class="card-body">
+                        <div class="d-flex mb-3">
+                            <div class="display-7">
+                                <i class="bi bi-building"></i>
+                                <!-- <i class="bi bi-credit-card-2-front"></i> -->
+                            </div>
+                            <div class="dropdown ms-auto">
+                                <!-- <a href="#" data-bs-toggle="dropdown" class="btn btn-sm" aria-haspopup="true"
+                                aria-expanded="false">
+                                    <i class="bi bi-three-dots"></i>
+                                </a> -->
+                                <div class="dropdown-menu dropdown-menu-end">
+                                    <a href="#" class="dropdown-item">View Detail</a>
+                                    <a href="#" class="dropdown-item">Download</a>
+                                </div>
+                            </div>
+                        </div>
+                        <h4 class="mb-3">Total Brands</h4>
+                        <div class="d-flex mb-3">
+                            <div class="display-7">{{$totalbrand}}</div>
+                            <div class="ms-auto" id="total-sales"></div>
+                        </div>
+                        <div class="text-secondary">
+                            Over last month 2.4% <i class="small bi bi-arrow-down"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <div class="col-lg-7 col-md-12">
                 <div class="card widget h-100">
                     <div class="card-header d-flex">
@@ -134,126 +254,7 @@
                     </div>
                 </div>
             </div>
-            <div class="col-lg-3 col-md-12">
-                <div class="card h-100">
-                    <div class="card-body">
-                        <div class="d-flex mb-3">
-                            <div class="display-7">
-                                <i class="bi bi-telephone-inbound"></i>
-                                <!-- <i class="bi bi-basket"></i> -->
-                            </div>
-                            <div class="dropdown ms-auto">
-                                <!-- <a href="#" data-bs-toggle="dropdown" class="btn btn-sm" aria-haspopup="true"
-                                aria-expanded="false">
-                                    <i class="bi bi-three-dots"></i>
-                                </a> -->
-                                <div class="dropdown-menu dropdown-menu-end">
-                                    <a href="#" class="dropdown-item">View Detail</a>
-                                    <a href="#" class="dropdown-item">Download</a>
-                                </div>
-                            </div>
-                        </div>
-                        <h4 class="mb-3">Total Leads</h4>
-                        <div class="d-flex mb-3">
-                            <div class="display-7">{{$totalead}}</div>
-                            <div class="ms-auto" id="total-orders"></div>
-                        </div>
-                        <div class="text-success">
-                            Over last month 1.4% <i class="small bi bi-arrow-up"></i>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-12">
-                <div class="card h-100">
-                    <div class="card-body">
-                        <div class="d-flex mb-3">
-                            <div class="display-7">
-                                <i class="bi bi-briefcase"></i>
-                                <!-- <i class="bi bi-credit-card-2-front"></i> -->
-                            </div>
-                            <div class="dropdown ms-auto">
-                                <!-- <a href="#" data-bs-toggle="dropdown" class="btn btn-sm" aria-haspopup="true"
-                                aria-expanded="false">
-                                    <i class="bi bi-three-dots"></i>
-                                </a> -->
-                                <div class="dropdown-menu dropdown-menu-end">
-                                    <a href="#" class="dropdown-item">View Detail</a>
-                                    <a href="#" class="dropdown-item">Download</a>
-                                </div>
-                            </div>
-                        </div>
-                        <h4 class="mb-3">Total Opportunities</h4>
-                        <div class="d-flex mb-3">
-                            <div class="display-7">{{$totalopportunity}}</div>
-                            <div class="ms-auto" id="total-opportunity"></div>
-                        </div>
-                        <div class="text-warning">
-                            Over last month 2.4% <i class="small bi bi-arrow-down"></i>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-12">
-                <div class="card h-100">
-                    <div class="card-body">
-                        <div class="d-flex mb-3">
-                            <div class="display-7">
-                                <i class="bi bi-briefcase"></i>
-                                <!-- <i class="bi bi-credit-card-2-front"></i> -->
-                            </div>
-                            <div class="dropdown ms-auto">
-                                <!-- <a href="#" data-bs-toggle="dropdown" class="btn btn-sm" aria-haspopup="true"
-                                aria-expanded="false">
-                                    <i class="bi bi-three-dots"></i>
-                                </a> -->
-                                <div class="dropdown-menu dropdown-menu-end">
-                                    <a href="#" class="dropdown-item">View Detail</a>
-                                    <a href="#" class="dropdown-item">Download</a>
-                                </div>
-                            </div>
-                        </div>
-                        <h4 class="mb-3">Total Packages</h4>
-                        <div class="d-flex mb-3">
-                            <div class="display-7">{{$totalpackage}}</div>
-                            <div class="ms-auto" id="total-opportunity"></div>
-                        </div>
-                        <div class="text-danger">
-                            Over last month 2.4% <i class="small bi bi-arrow-down"></i>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-12">
-                <div class="card h-100">
-                    <div class="card-body">
-                        <div class="d-flex mb-3">
-                            <div class="display-7">
-                                <i class="bi bi-building"></i>
-                                <!-- <i class="bi bi-credit-card-2-front"></i> -->
-                            </div>
-                            <div class="dropdown ms-auto">
-                                <!-- <a href="#" data-bs-toggle="dropdown" class="btn btn-sm" aria-haspopup="true"
-                                aria-expanded="false">
-                                    <i class="bi bi-three-dots"></i>
-                                </a> -->
-                                <div class="dropdown-menu dropdown-menu-end">
-                                    <a href="#" class="dropdown-item">View Detail</a>
-                                    <a href="#" class="dropdown-item">Download</a>
-                                </div>
-                            </div>
-                        </div>
-                        <h4 class="mb-3">Total Brands</h4>
-                        <div class="d-flex mb-3">
-                            <div class="display-7">{{$totalbrand}}</div>
-                            <div class="ms-auto" id="total-sales"></div>
-                        </div>
-                        <div class="text-secondary">
-                            Over last month 2.4% <i class="small bi bi-arrow-down"></i>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            
             <!-- <div class="col-lg-4 col-md-12">
                 <div class="card h-100">
                     <div class="card-body">
